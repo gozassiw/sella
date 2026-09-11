00c1cb7ce6e5aee79f85914e9922ea6de7e9290f
gozassiw <3.22287117e+08+gozassiw@users.noreply.github.com>
2026-09-11 18:53:01 +0000

    Improve public page performance

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: { ignoreDuringBuilds: true },
+  poweredByHeader: false,
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "**.supabase.co" },
+      { protocol: "https", hostname: "**.supabase.in" },
+    ],
+  },
 };
 module.exports = nextConfig;
